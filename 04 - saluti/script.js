6b3bcc940acf08c61388641c1ac687451fa67892
Denny Rubino <rubidenny@gmail.com>
2024-12-05 15:21:55 +0100

    add snack 4

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,10 +4,14 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function hello(name) {
+    return ("Ciao" + " " + {name})
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
-
+function hello(userName) {
+    return ("Ciao" + " " + userName)
+}
+console.log(hello(userName));
 //Risultato atteso se si passa 'Mario': // ciao Mario
