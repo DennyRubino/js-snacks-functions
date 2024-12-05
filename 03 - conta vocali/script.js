8a17bd528a360293277fc54022405c23d5d83acb
Denny Rubino <rubidenny@gmail.com>
2024-12-06 00:02:42 +0100

    add snack3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -2,12 +2,15 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
-
+function countVocal(word) {
+    const vocals = ['a','e','i','o','u']
+    return word.split("").filter(word => vocals.includes(word))
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const count = countVocal(word);
+console.log(count);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
